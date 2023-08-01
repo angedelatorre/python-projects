71fbae168d177569a45d35908b519fe871b8d464
angedelatorre <79916507+angedelatorre@users.noreply.github.com>
2023-08-01 18:20:22 +0800

    bmi calculator

---
+++ BMI_Calculator.py
@@ -0,0 +1,10 @@
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+new_height = float(height)
+bmi = new_weight / (new_height**2)
+
+print(new_bmi)
+new_bmi = int(bmi)
+print(new_bmi)
